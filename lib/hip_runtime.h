7559a56589548a92cda39770c9121b29ad4626eb
Michal Babej <michal.babej@tuni.fi>
2019-12-23 17:13:12 +0200

    Add vector types. Initially just uses Clang extended vectors

---
+++ lib/hip_runtime.h
@@ -0,0 +1,14 @@
+
+#ifndef HIPCL_HIP_RUNTIME_H
+#define HIPCL_HIP_RUNTIME_H
+
+#ifndef __HIP_PLATFORM_HIPCL__
+#define __HIP_PLATFORM_HIPCL__
+#endif
+
+#include <hip/hipcl.hh>
+
+#include <hip/hip_vector_types.h>
+
+#endif
+
