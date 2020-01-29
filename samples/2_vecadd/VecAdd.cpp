21b42d00423c5c322e8f1527155c8dd6d0cca93e
Michal Babej <michal.babej@tuni.fi>
2020-01-29 14:29:11 +0200

    Update VecAdd example with code from Zheming Jin <zjin@anl.gov>

--- samples/2_vecadd/VecAdd.cpp
+++ samples/2_vecadd/VecAdd.cpp
@@ -68,6 +68,23 @@ bool compareRes4(size_t i, bool print, T res1, T res2)
 }
 
 template <typename T, typename CMPT>
+bool compareRes3(size_t i, bool print, T res1, T res2) {
+  CMPT res = (res1 != res2);
+  if (res.x + res.y + res.z == 0)
+    return true;
+
+  if (print) {
+    std::cerr << "FAIL AT: " << i << "\n";
+    std::cerr << "CMP: " << res.x << " " << res.y << " " << res.z << "\n";
+
+    std::cerr << "CPU: " << res1.x << " " << res1.y << " " << res1.z << "\n";
+
+    std::cerr << "GPU: " << res2.x << " " << res2.y << " " << res2.z << "\n";
+  }
+  return false;
+}
+
+template <typename T, typename CMPT>
 bool compareRes2(size_t i, bool print, T res1, T res2)
 {
   CMPT res = (res1 != res2);
@@ -88,6 +105,25 @@ bool compareRes2(size_t i, bool print, T res1, T res2)
   return false;
 }
 
+/*
+template <typename T, typename CMPT>
+bool compareRes1(size_t i, bool print, T res1, T res2)
+{
+  CMPT res = (res1 != res2);
+
+  if (res.x == 0)
+    return true;
+
+  if (print) {
+      std::cerr << "FAIL AT: " << i << "\n";
+      std::cerr << "CMP: " << res.x << "\n";
+
+      std::cerr << "CPU: " << res1.x << "\n";
+      std::cerr << "GPU: " << res2.x << "\n";
+  }
+  return false;
+}
+*/
 
 #define ERR_CHECK_2 \
   do { \
@@ -201,15 +237,98 @@ int main() {
   hipGetDeviceProperties(&devProp, 0);
   std::cout << "Device name " << devProp.name << std::endl;
 
-  std::cout << "FLOAT4 test\n";
+  std::cout << "float4 test\n";
   float4 m_f4 = make_float4(7.0f, 7.0f, 7.0f, 7.0f);
   TestVectors<float4, RandomGenFuncFloat>(rndf, m_f4, compareRes2<float4, int4>);
 
-  std::cout << "INT2 test\n";
+  std::cout << "float3 test\n";
+  float3 m_f3 = make_float3(7.0f, 7.0f, 7.0f);
+  TestVectors<float3, RandomGenFuncFloat>(rndf, m_f3,
+                                          compareRes2<float3, int3>);
+
+  std::cout << "float2 test\n";
+  float2 m_f2 = make_float2(7.0f, 7.0f);
+  TestVectors<float2, RandomGenFuncFloat>(rndf, m_f2,
+                                          compareRes2<float2, int2>);
+
+  // std::cout << "float1 test\n";
+  // float1 m_f1 = make_float1(22);
+  // TestVectors<float1, RandomGenFuncInt>(rndi, m_f1, compareRes1<float1,
+  // int1>);
+
+  std::cout << "int4 test\n";
+  int4 m_i4 = make_int4(3, 17, 48, 29);
+  TestVectors<int4, RandomGenFuncInt>(rndi, m_i4, compareRes4<int4, int4>);
+
+  std::cout << "int3 test\n";
+  int3 m_i3 = make_int3(3, 17, 38);
+  TestVectors<int3, RandomGenFuncInt>(rndi, m_i3, compareRes3<int3, int3>);
+
+  std::cout << "int2 test\n";
   int2 m_i2 = make_int2(22, 19);
   TestVectors<int2, RandomGenFuncInt>(rndi, m_i2, compareRes2<int2, int2>);
 
-  std::cout << "INT4 test\n";
-  int4 m_i4 = make_int4(3, 17, 48, 29);
-  TestVectors<int4, RandomGenFuncInt>(rndi, m_i4, compareRes4<int4, int4>);
+  // std::cout << "uint1 test\n";
+  // uint1 m_i1 = make_uint1(22);
+  // TestVectors<uint1, RandomGenFuncInt>(rndi, m_i1, compareRes1<uint1, int1>);
+
+  std::cout << "ulong4 test\n";
+  ulong4 m_ul4 = make_ulong4(3, 17, 48, 29);
+  TestVectors<ulong4, RandomGenFuncInt>(rndi, m_ul4,
+                                        compareRes4<ulong4, long4>);
+
+  std::cout << "ulong3 test\n";
+  ulong3 m_ul3 = make_ulong3(3, 17, 38);
+  TestVectors<ulong3, RandomGenFuncInt>(rndi, m_ul3,
+                                        compareRes3<ulong3, long3>);
+
+  std::cout << "ulong2 test\n";
+  ulong2 m_ul2 = make_ulong2(22, 19);
+  TestVectors<ulong2, RandomGenFuncInt>(rndi, m_ul2,
+                                        compareRes2<ulong2, long2>);
+
+  // std::cout << "ulong1 test\n";
+  // ulong1 m_ul1 = make_ulong1(22);
+  // TestVectors<ulong1, RandomGenFuncInt>(rndi, m_ul1, compareRes1<ulong1,
+  // long1>);
+
+  std::cout << "ulonglong4 test\n";
+  ulonglong4 m_ull4 = make_ulonglong4(3, 17, 48, 29);
+  TestVectors<ulonglong4, RandomGenFuncInt>(rndi, m_ull4,
+                                            compareRes4<ulonglong4, long4>);
+
+  std::cout << "ulonglong3 test\n";
+  ulonglong3 m_ull3 = make_ulonglong3(3, 17, 38);
+  TestVectors<ulonglong3, RandomGenFuncInt>(rndi, m_ull3,
+                                            compareRes3<ulonglong3, long3>);
+
+  std::cout << "ulonglong2 test\n";
+  ulonglong2 m_ull2 = make_ulonglong2(22, 19);
+  TestVectors<ulonglong2, RandomGenFuncInt>(rndi, m_ull2,
+                                            compareRes2<ulonglong2, long2>);
+
+  // std::cout << "ulonglong1 test\n";
+  // ulonglong1 m_ull1 = make_ulonglong1(22);
+  // TestVectors<ulonglong1, RandomGenFuncInt>(rndi, m_ull1,
+  // compareRes1<ulonglong1, long1>);
+
+  std::cout << "uchar4 test\n";
+  uchar4 m_uc4 = make_uchar4(3, 17, 48, 29);
+  TestVectors<uchar4, RandomGenFuncInt>(rndi, m_uc4,
+                                        compareRes4<uchar4, char4>);
+
+  std::cout << "uchar3 test\n";
+  uchar3 m_uc3 = make_uchar3(3, 17, 38);
+  TestVectors<uchar3, RandomGenFuncInt>(rndi, m_uc3,
+                                        compareRes3<uchar3, char3>);
+
+  std::cout << "uchar2 test\n";
+  uchar2 m_uc2 = make_uchar2(22, 19);
+  TestVectors<uchar2, RandomGenFuncInt>(rndi, m_uc2,
+                                        compareRes2<uchar2, char2>);
+
+  // std::cout << "uchar1 test\n";
+  // uchar1 m_uc1 = make_uchar1(11);
+  // TestVectors<uchar1, RandomGenFuncInt>(rndi, m_uc1, compareRes1<uchar1,
+  // char1>);
 }
